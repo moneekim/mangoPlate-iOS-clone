9113caf28721b3e2098620ad30b1ec0576627319
MoKyungKim <62017268+MoKyungKim@users.noreply.github.com>
2021-08-19 05:59:40 +0900

    Initial Commit

---
+++ MangoPlate-iOS-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MangoPlate-iOS-clone
+//
+//  Created by 김모경 on 2021/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
